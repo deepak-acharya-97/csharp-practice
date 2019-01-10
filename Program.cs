8d67a147598b404f529019c955179836a6f1ed1e
Deepak Acharya <aacharyadeepak100@gmail.com>
2019-01-10 15:29:19 +0530

    Implicit default constructor tested
    Although we can't define it explicitly

--- Program.cs
+++ Program.cs
@@ -4,16 +4,21 @@ using System.Collections.Generic;
 
 namespace play_with_struct
 {
-    struct Student 
+    struct Student
     {
         public string RollNumber;
         public string Name;
 
         public Student(string RollNumber, string Name)
         {
-            Console.WriteLine("I'm called only with new keyword");
-            this.RollNumber=RollNumber;
-            this.Name=Name;
+            Console.WriteLine("I'm called only with new keyword with rollnumber and name passed as arguments");
+            this.RollNumber = RollNumber;
+            this.Name = Name;
+        }
+
+        public override string ToString()
+        {
+            return $"***\nName : {this.Name}\nRoll Number: {this.RollNumber}\n***";
         }
         // public string OfficialEmailId;
         // public string Address;
@@ -33,12 +38,30 @@ namespace play_with_struct
     {
         static void Main(string[] args)
         {
-            Student[] students=new Student[10];
+            Student[] students = new Student[10];
             Student s;
-            s.RollNumber="1";
-            s.Name="Deepak Acharya"; // commenting this will be error as all properties must be initialised before using it (line 42)
-            Student s1= new Student("14IT210", "Deepak Acharya");
-            students[0]=s;
+            s.RollNumber = "1";
+            s.Name = "Deepak Acharya"; // commenting this will be error as all properties must be initialised before using it (line 42)
+            Student s1 = new Student("14IT210", "Deepak Acharya");
+            Student s2 = new Student(); //Although explicitly can't define default constructor we can pass no args with new keyword.
+            Console.WriteLine(s1);
+            Console.WriteLine(s2);
+            students[0] = s2;
+
+            //Output
+
+            // $ dotnet run
+            // I'm called only with new keyword with rollnumber and name passed as arguments
+            // * **
+            // Name : Deepak Acharya
+            // Roll Number: 14IT210
+            // ***
+            // ***
+            // Name :
+            // Roll Number:
+            // ***
+
+
         }
     }
 }
